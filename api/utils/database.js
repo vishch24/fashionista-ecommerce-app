881f05e3a8d9d965d26f14a0926aa54568306dc5
Vishakha <vishakha926@gmail.com>
2026-09-04 15:59:23 +0000

    Resolved SSL issue

--- api/utils/database.js
+++ api/utils/database.js
@@ -4,10 +4,12 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
   dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
+    ssl: process.env.DATABASE_URL.includes("sslmode=disable")
+      ? false
+      : {
+          require: true,
+          rejectUnauthorized: false,
+        },
   },
 });
 
